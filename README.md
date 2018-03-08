12507c5594c6c7d123aa43ad7f4e08a0b1d3d843
Brady Holt <brady.holt@gmail.com>
2018-03-08 14:02:56 -0600

    Use caporal; rename to ynab-utils

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
-# ynab-amazon-memos
+# ynab-utils
 
-Updates Amazon purchase transactions in YNAB with a memo listing the order items
+Various utilities for YNAB
+
+## Usage
+
+```
+USAGE
+
+     index.ts <command> [options]
+
+   COMMANDS
+
+     importTransactions <email> <password> <account_id>                   Import transactions for an account
+     updateAmazonMemos <email> <password> <budget_id> <access_token>      Update Amazon transactions in YNAB with list of order items
+     help <command>                                                       Display help for a specific command
+
+   GLOBAL OPTIONS
+
+     -h, --help         Display help
+     -V, --version      Display version
+     --no-color         Disable colors
+     --quiet            Quiet mode - only displays warn and error messages
+     -v, --verbose      Verbose mode - will also output debug messages
+```
 
 ## Deploy
 
--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,39 @@
 import * as moment from "moment";
-import * as program from "commander";
+import * as program from "caporal";
 
 import { AmazonOrderFetcher } from "./amazonOrderFetcher";
 import { ynabMemoUpdator } from "./ynabMemoUpdator";
 import { ynabTransactionImporter } from "./ynabTransactionImporter";
 
+program.version("1.0.0");
+
 program
-  .version("0.1.0")
-  .option("--amazon-email <email>", "Amazon Account Email Address")
-  .option("--amazon-password <password>", "Amazon Account Password")
-  .option("--ynab-email <email>", "YNAB Account Email Address")
-  .option("--ynab-password <password>", "YNAB Account Password")
-  .option("--ynab-access-token <access_token>", "YNAB API Access Token")
-  .option("--ynab-budget-id <budget_id>", "YNAB Budget Id")
-  .option(
-    "-ai, --ynab-account-ids <account_ids>",
-    "YNAB Account Ids (comma delimited)"
-  )
-  .parse(process.argv);
+  .command("importTransactions", "Import transactions for an account")
+  .argument("<email>", "YNAB Account Email Address")
+  .argument("<password>", "YNAB Account Password")
+  .argument("<account_id>", "YNAB Account Id")
+  .action(async args => {
+    const transactionImporter = new ynabTransactionImporter(args.email, args.password);
+    await transactionImporter.importTransactions(args.accountId);
+  });
 
-// If any require options are not provided print help and exit
-if (
-  !program.amazonEmail ||
-  !program.amazonPassword ||
-  !program.ynabEmail ||
-  !program.ynabPassword ||
-  !program.ynabAccessToken ||
-  !program.ynabBudgetId ||
-  !program.ynabAccountIds
-) {
-  program.help();
-}
+program
+  .command("updateAmazonMemos", "Update Amazon transactions in YNAB with list of order items")
+  .argument("<email>", "Amazon Account Email Address")
+  .argument("<password>", "Amazon Account Password")
+  .argument("<budget_id>", "YNAB Budget Id")
+  .argument("<access_token>", "YNAB API Access Token")
+  .action(async args => {
+    let fromDate = moment()
+      .subtract(1, "month")
+      .toISOString();
+    let toDate = moment().toISOString();
 
-(async () => {
-  // Import YNAB Transactions
-  const transactionImporter = new ynabTransactionImporter(
-    program.ynabEmail,
-    program.ynabPassword
-  );
-  await transactionImporter.importTransactions(
-    program.ynabAccountIds.split(",")
-  );
+    const amazonOrderFetcher = new AmazonOrderFetcher(args.email, args.password);
+    const amazonOrders = await amazonOrderFetcher.getOrders(fromDate, toDate);
 
-  // Fetch Amazon Orders
-  let fromDate = moment()
-    .subtract(1, "month")
-    .toISOString();
-  let toDate = moment().toISOString();
+    const memoUpdator = new ynabMemoUpdator(args.accessToken);
+    await memoUpdator.updateAmazonTransactionMemos(args.budgetId, amazonOrders);
+  });
 
-  const amazonOrderFetcher = new AmazonOrderFetcher(
-    program.amazonEmail,
-    program.amazonPassword
-  );
-  const amazonOrders = await amazonOrderFetcher.getOrders(fromDate, toDate);
-
-  // Update Amazon Transaction memos
-  const memoUpdator = new ynabMemoUpdator(program.ynabAccessToken);
-  await memoUpdator.updateAmazonTransactionMemos(
-    program.ynabBudgetId,
-    amazonOrders
-  );
-
-  process.exit(0);
-})();
+program.parse(process.argv);
