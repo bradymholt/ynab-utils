a52cbdbe364f00ee20e1890a4f6c5da28e4414db
Brady Holt <brady.holt@gmail.com>
2020-04-28 15:43:45 -0500

    Use new transaction import endpoint to import

--- src/transactionImporter.ts
+++ src/transactionImporter.ts
@@ -1,49 +1,14 @@
-import * as puppeteer from "puppeteer";
-import * as config from "../config.json";
+import * as ynab from "ynab";
+import config from "../config.json";
 
 export class transactionImporter {
+  ynabAPI: ynab.api;
+
+  constructor() {
+    this.ynabAPI = new ynab.api(config.personal_access_token);
+  }
+
   public async run() {
-    // Setup
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1366, height: 768 });
-
-    await this.login(page);
-    await this.importForAllAccounts(page);
-    await browser.close();
-  }
-
-  private async login(page: puppeteer.Page) {
-    const loginPath = "https://app.youneedabudget.com/users/authentication";
-
-    console.log(`Logging into YNAB...`);
-    await page.goto(loginPath);
-    await page.type("#request_data_email", config.ynab_web_email);
-    await page.type("#request_data_password", config.ynab_web_password);
-    await page.click("#login");
-    await this.delay(10000);
-  }
-
-  private async importForAllAccounts(page: puppeteer.Page) {
-    let accountAnchors = await page.$$("a.nav-account-row");
-
-    for (const anchor of accountAnchors) {
-      await anchor.click();
-      await this.delay(2000);
-      try {
-        console.log(`Importing transactions on: ${page.url()}`);
-        await page.click(".accounts-toolbar-import-transactions");
-        // Wait for import and server sync
-        await this.delay(5000);
-      } catch (error) {
-        // We expect an error for Unlinked accounts (.accounts-toolbar-import-transactions does not exist) so just ignore them.
-      }
-    }
-  }
-
-  private delay(milliseconds: number) {
-    return new Promise((resolve, reject) => {
-      setTimeout(resolve, milliseconds);
-    });
+    await this.ynabAPI.transactions.importTransactions(config.budget_id);
   }
 }
